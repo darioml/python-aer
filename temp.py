10c7683599be4c5314cdeff0c8df5161ea180c68
= <dario_dml@hotmail.com>
2015-05-29 18:44:57 +0100

    Added unpack data

--- temp.py
+++ temp.py
@@ -3,12 +3,15 @@ import numpy as np
 
 import os, datetime
 
-from pprint import pprint
 
 class pAER:
 	def __init__(self, filename, max_events=1e6):
+		self.filename = filename
+		self.max_events = max_events
+		self.load()
 
-		with open(filename, 'r') as f:
+	def load(self):
+		with open(self.filename, 'r') as f:
 			line = f.readline()
 			while (line[0] == '#'):
 				if (line[0:9] == '#!AER-DAT'):
@@ -23,30 +26,39 @@ class pAER:
 			f.seek(0,2)
 			numEvents = math.floor( ( f.tell() - current ) / 8 )
 			
-			if numEvents > max_events:
-				print 'There are %i events, but max_events is set to %i. Will only use %i events.' % (numEvents, max_events, max_events)
-				numEvents = max_events
+			if numEvents > self.max_events:
+				print 'There are %i events, but max_events is set to %i. Will only use %i events.' % (numEvents, self.max_events, self.max_events)
+				numEvents = self.max_events
 
 			f.seek(current)
 
-			aerTimestamp = np.zeros( numEvents )
-			aerData      = np.zeros( numEvents )
+			self.timestamps = np.zeros( numEvents )
+			self.data       = np.zeros( numEvents )
 
 			for i in range(numEvents):
-				
-				aerTimestamp[i] = int(f.read(4).encode('hex'), 16)
-				aerData[i]      = int(f.read(4).encode('hex'), 16)
 
-			pprint(aerData)
-			pprint(aerTimestamp)
+				self.data[i] = int(f.read(4).encode('hex'), 16)
+				self.timestamps[i]       = int(f.read(4).encode('hex'), 16)
 
+			return self
 
-
+	def unpackData(self, data):
+		noData = len(data)
 		
-		print
-		print max_events
+		x = np.zeros(noData)
+		y = np.zeros(noData)
+		t = np.zeros(noData)
+
+		for i in range(1):
+			d = int(data[i])
+			
+			t[i] = d & 0x1			
+			x[i] = (d >> 0x1) & 0x7F
+			y[i] = (d >> 0x8) & 0x7F
+		return (x,y,t)
 
 
 if __name__ == "__main__":
-	hello = pAER('/Users/darioml/src/fyp-aedata-matlab/test.aedat', max_events=2)
-	print "test"
+	hello = pAER('/Users/darioml/src/fyp-aedata-matlab/test.aedat', max_events=10)
+
+	(x,y,t) = hello.unpackData(hello.data)
